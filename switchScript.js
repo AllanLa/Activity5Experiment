1effaa17e91700c3d6e9997f3088be5b8ab64c28
Allan La <al037@bucknell.edu>
2016-06-16 16:01:39 -0400

    Morphing Feedback

--- switchScript.js
+++ switchScript.js
@@ -53,8 +53,9 @@ function generateStacked(){
     .append("svg")
 	.style('background', '#C9D7D6')    
     .attr("width", w)
-    .attr("height", h )
+    .attr("height", h)
     .append("svg:g")
+    .attr("align", "center")
     .attr("transform", "translate(25,470)");
 
     x = d3.scale.ordinal().rangeRoundBands([0, w-50])
@@ -97,10 +98,21 @@ function generateStacked(){
     .attr("x", function(d) { return x(d.x); })
     .attr("y", function(d) { return -y(d.y0) - y(d.y); })
     .attr("height", function(d) { return y(d.y); })
-    .attr("width", x.rangeBand())
-    .attr("align", "center");
+    .attr("width", x.rangeBand());
+/*
+	Used to create X Axis
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .tickSize(0)
+	    .orient("bottom");
+
+    svg.append("g")
+    .attr("class", "x axis")
+    .call(xAxis);
+*/
 
     d3.select("#chart").attr("align","center");   
+    d3.selectAll("g.valgroup").attr("align", "center");
 
     generateStackedDots();
 }			
@@ -109,6 +121,7 @@ function generateStackedDots(){
 	//Only for stacked bargraphs
 	var together = d3.selectAll("rect");;
 	var arrayOfIndividuals = together[0];
+
 
 	//Picked two random parts of the rectangle
 	ranNum1 = Math.floor(Math.random() * arrayOfIndividuals.length);
@@ -120,6 +133,12 @@ function generateStackedDots(){
 
 	rectangle1 = arrayOfIndividuals[ranNum1];
 	rectangle2 = arrayOfIndividuals[ranNum2];
+
+	d3.select(rectangle1).style("fill", "green");
+	d3.select(rectangle2).style("fill", "green");
+
+	value1 = d3.select(rectangle1).data()[0].y
+	value2 = d3.select(rectangle2).data()[0].y
 
 	//after selecting rectangles will now add circles to the rectangles
 	width1 = d3.select(rectangle1).attr("width");
@@ -165,66 +184,9 @@ function generateStackedDots(){
     	currentCorrectAnswer = ((area2 / area1) * 100).toFixed(2);
     }
 
+   generateAdjacentBars(value1, value2);
 }			
 
-function generateBarDots(){
-	//Only for rectangles, together selects all the Rectangles and puts it into
-	//a nested array like [Array[8]], so gotta extract the children
-	var together = d3.selectAll("rect");;
-	var arrayOfIndividuals = together[0];
-
-	//Picked two random parts of the rectangle
-	ranNum1 = Math.floor(Math.random() * arrayOfIndividuals.length);
-	ranNum2 = Math.floor(Math.random() * arrayOfIndividuals.length);
-
-	while ( ranNum1 == ranNum2) {
-		ranNum1 = Math.floor(Math.random() * arrayOfIndividuals.length);
-	}
-
-	rectangle1 = arrayOfIndividuals[ranNum1];
-	rectangle2 = arrayOfIndividuals[ranNum2];
-
-
-	//after selecting rectangles will now add circles to the rectangles
-	width1 = d3.select(rectangle1).attr("width");
-	height1 = d3.select(rectangle1).attr("height");
-	x1 = d3.select(rectangle1).attr("x");
-	y1 = d3.select(rectangle1).attr("y");
-
-	width2 = d3.select(rectangle2).attr("width");
-	height2 = d3.select(rectangle2).attr("height");
-	x2 = d3.select(rectangle2).attr("x");
-	y2 = d3.select(rectangle2).attr("y");
-
-	area1 = width1 * height1;
-	area2 = width2 * height2;
-
-	d3.select("svg")
-	.append("circle")
-    .style("stroke", "gray")
-    .style("fill", "black")
-    .attr("r", 5)
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top +')')
-    .attr("cx", parseInt(x1) + parseInt(30))
-    .attr("cy", parseInt(y1) - parseInt(10));
-
-	d3.select("svg")
-	.append("circle")
-    .style("stroke", "gray")
-    .style("fill", "black")
-    .attr("r", 5)
-    .attr('transform', 'translate(' + margin.left + ', ' + margin.top +')')
-    .attr("cx", parseInt(x2) + parseInt(30))
-    .attr("cy", parseInt(y2) - parseInt(10));
-
-    if(area1 < area2){
-    	currentCorrectAnswer = ((area1 / area2) * 100).toFixed(2);
-    }
-    else{
-    	currentCorrectAnswer = ((area2 / area1) * 100).toFixed(2);
-    }
-
-}
 
 function generatePieChart(){
 	//Referenced from https://gist.github.com/enjalot/1203641
@@ -286,6 +248,7 @@ function generatePieChart(){
 	slice1 = arrayOfSlices[ranNum1];
 	slice2 = arrayOfSlices[ranNum2];
 
+
 	d3.select(slice1).append("circle")
 		.style("stroke", "gray")
 		.style("fill", "black")
@@ -308,18 +271,23 @@ function generatePieChart(){
         return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
     })
 
-	area1= d3.select(slice1).data()[0].value
-	area2= d3.select(slice2).data()[0].value                
+ 	d3.select(slice1).select("path").attr("fill", "green")        
+    d3.select(slice2).select("path").attr("fill", "green") 	
+
+	value1= d3.select(slice1).data()[0].value
+	value2= d3.select(slice2).data()[0].value                
 
     d3.select("#chart").attr("align","center");
 
 
-    if(area1 < area2){
-    	currentCorrectAnswer = ((area1 / area2) * 100).toFixed(2);
+    if(value1 < value2){
+    	currentCorrectAnswer = ((value1 / value2) * 100).toFixed(2);    	
     }
     else{
-    	currentCorrectAnswer = ((area2 / area1) * 100).toFixed(2);
+    	currentCorrectAnswer = ((value2 / value1) * 100).toFixed(2);
     }
+
+    generateAdjacentBars(value1, value2);
 }
 
 function generateBarGraph(){
@@ -471,9 +439,229 @@ function generateBarGraph(){
 			.style({fill: 'none', stroke: "#000"})
 
 
-	d3.select("#chart").attr("align","center"); 	
+	d3.select("#chart").attr("align","center");	
 
 	generateBarDots();
+}
+
+function generateBarDots(){
+	//Only for rectangles, together selects all the Rectangles and puts it into
+	//a nested array like [Array[8]], so gotta extract the children
+	var together = d3.selectAll("rect");;
+	var arrayOfValues = d3.selectAll("rect").data()
+	var arrayOfIndividuals = together[0];
+
+	//Picked two random parts of the rectangle
+	ranNum1 = Math.floor(Math.random() * arrayOfIndividuals.length);
+	ranNum2 = Math.floor(Math.random() * arrayOfIndividuals.length);
+
+	while ( ranNum1 == ranNum2) {
+		ranNum1 = Math.floor(Math.random() * arrayOfIndividuals.length);
+	}
+
+	rectangle1 = arrayOfIndividuals[ranNum1];
+	rectangle2 = arrayOfIndividuals[ranNum2];
+
+	d3.select(rectangle1).style("fill", "green");
+	d3.select(rectangle2).style("fill", "green");
+
+	if(ranNum1 < ranNum2){
+		value1 = arrayOfValues[ranNum1];
+		value2 = arrayOfValues[ranNum2];
+	}
+
+	else{
+		value1 = arrayOfValues[ranNum2];
+		value2 = arrayOfValues[ranNum1];		
+	}
+
+
+	//after selecting rectangles will now add circles to the rectangles
+	width1 = d3.select(rectangle1).attr("width");
+	height1 = d3.select(rectangle1).attr("height");
+	x1 = d3.select(rectangle1).attr("x");
+	y1 = d3.select(rectangle1).attr("y");
+
+	width2 = d3.select(rectangle2).attr("width");
+	height2 = d3.select(rectangle2).attr("height");
+	x2 = d3.select(rectangle2).attr("x");
+	y2 = d3.select(rectangle2).attr("y");
+
+	area1 = width1 * height1;
+	area2 = width2 * height2;
+
+	d3.select("svg")
+	.append("circle")
+    .style("stroke", "gray")
+    .style("fill", "black")
+    .attr("r", 5)
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top +')')
+    .attr("cx", parseInt(x1) + parseInt(30))
+    .attr("cy", parseInt(y1) - parseInt(10));
+
+	d3.select("svg")
+	.append("circle")
+    .style("stroke", "gray")
+    .style("fill", "black")
+    .attr("r", 5)
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top +')')
+    .attr("cx", parseInt(x2) + parseInt(30))
+    .attr("cy", parseInt(y2) - parseInt(10));
+
+    if(area1 < area2){
+    	currentCorrectAnswer = ((area1 / area2) * 100).toFixed(2);
+    }
+    else{
+    	currentCorrectAnswer = ((area2 / area1) * 100).toFixed(2);
+    }
+
+    generateAdjacentBars(value1, value2);
+
+}
+
+function generateAdjacentBars(value1, value2){
+
+
+	var barData = [value1, value2];
+
+	
+	var height = 200 - margin.top - margin.bottom,
+		width = 300 - margin.left - margin.right,
+		barWidth = 50,
+		varOffset = 5;
+
+	var yScale = d3.scale.linear()
+			.domain([0, d3.max(barData)])
+			.range([0, height])
+	/*
+	the domain is the original information,
+	the range is the thing we want to fit it into,
+	remaps the Y value so that everything will fit into the
+	size of the chart, using a quantitative scale
+	*/
+
+	var xScale = d3.scale.ordinal()
+			.domain(d3.range(0,barData.length))
+			.rangeBands([0, width])
+			//.rangeBands([0, width]), .2), the .2 gives padding between each bar
+	/*
+	ordinal scales are used to scale horizontally, essentially the
+	horizontal positioning of the data, rangeBands allows more
+	control with the padding between elements and width of the
+	element as well, use rangeBands to set the ordinal scale
+	and rangeBand to find the width of the element
+	*/
+
+
+	/*would use bardata.length isntead of max(barData) if want to
+	change color depending on horizontal value */
+
+	/*Color scale, so basically the smaller the data, it will be the color
+	on the left, whereas the larger the data, the closer it will be to the
+	right, so color changes depending on the Y value*/
+
+	var toolTip = d3.select('body').append('div')
+		.style('position', 'absolute')
+		.style('padding', '0 10px')
+		.style('background', 'white')
+		.style('opacity', 0)
+
+	/*position = absolute will allow toolTip to follow
+	in relation to the page
+	padding would be 0 px on the top and bottom, and 10
+	from side to side*/
+
+	var chart = d3.select('#chart2').append('svg')
+		.style('background', 'white')
+	    .attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.style('background', '#C9D7D6')
+		.append('g')
+		//moves graphic over x px using margin.left and y using margin.top
+		.attr('transform', 'translate(' + margin.left + ', ' + margin.top +')')
+		.selectAll('rect').data(barData)
+		.enter().append('rect')
+			.style('fill', 'none')  //so over here would create a function
+			.style("stroke", "black")
+			.attr('width', xScale.rangeBand())//passing in colors(i) for horizontal change;
+			.attr('height', function(d){
+				return d;
+			})
+			.attr('x', function(d, i){
+				return xScale(i);
+			})
+	/*
+	like how d is passed in as an element from barData, i is also passed
+	in as the index of the element from the data
+	*/		
+			.attr('y' , height)
+
+	/*
+	selects the chart ID from the html document, adds a <svg>
+	tag after it with the cavas 400 x 600, so the selectAll('rect') selects
+	all the rectangles that don't exist yet, but we are creating rectangles
+	which then it selects it, then it styles it up, etc.
+	*/
+
+
+	chart.transition()
+		.attr('height', function(d){
+			return yScale(d);
+		})
+		.attr('y', function(d,i){
+			return height - yScale(d);
+		})
+		.delay(function(d,i){
+			return i * 10;
+		})
+		.duration(1000) //slows down the drawing of each bar by a second
+		.ease('elastic') //makes it bounce up and down
+
+	var chart2 = d3.select("#chart2").select("svg")[0][0];
+
+	var yGuideScale = d3.scale.linear()
+		.domain([0, d3.max(barData)])
+		.range([height, 0]) // makes the range backgrounds so y axis looks right
+
+	var yAxis = d3.svg.axis()
+		.scale(yGuideScale)
+		.orient('left') //allignment of the scale
+		.tickFormat(""); //so 10 ticks
+
+	//used to actually add the y-axis to the chart
+	var yGuide = d3.select(chart2).append('g')
+		yAxis(yGuide)
+		//creates the y-axis based on the chart, not hardcoded!
+		yGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top +')') //move 25 px right, 10 pixel down, we wouldnt have seen the y-axis cause to far to the left
+		yGuide.selectAll('path')
+			.style({fill: 'none', stroke: "#000"})
+
+
+
+	//creates the x axis for us
+	var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient('bottom')
+		.tickFormat("")
+		.tickValues(xScale.domain().filter(function(d,i){
+			return !(i % (barData.length / 4));
+		}))/*the way to create x ticks is because
+			initially we might not know how many values we have for the
+			x axis, could be 10 ticks, or 20 ticks, so the way to do
+			this is with the function. The filter prevents a tick
+			on every single bar so we use that*/
+
+	//actually adds the x axis to the chart
+
+	var xGuide = d3.select(chart2).append('g')
+		xAxis(xGuide)
+		xGuide.attr('transform', 'translate(' + margin.left + ', ' + 
+				(margin.top + height) +')')
+		xGuide.selectAll('path')
+			.style({fill: 'none', stroke: "#000"})
+
+
+	d3.select("#chart2").attr("align","center");	
 }
 
 function generateText(){
@@ -521,6 +709,7 @@ function nextChart(){
 
 	currentQuestion += 1;
 	d3.select("svg").remove(); //Will get rid of the current chart
+	d3.select("#chart2").select("svg").remove() //get rid of the second chart
 
 	if (currentQuestion > TOTALQUESTIONS){
 		endSurvey();
@@ -558,9 +747,10 @@ function endSurvey(){
 	d3.select(".container").remove();
 	d3.select("div").classed("wrapper", true).html("Thanks for your participation!");	
 	
+	//pushes an object with the userID, and the arrayOfAnswers into the array of Users
 	users.push({user: userID, arrayOfAnswers: answers});	
 	//for some reason, if I do not have an error, the webpage just freezes
-	document.clea();
+	//document.clea();
 }
 
 
